03589e83a57b451c8f73a51ca5962d5b219c1b11
DaHerdi <danendra.herdian1243@gmail.com>
2022-12-09 11:47:20 +0700

    initiate testimoni

---
+++ lib/screens/Testimoni/add_testimoni_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:bidcare/model/testimoni_model.dart';
+import 'package:bidcare/screens/Testimoni/testmoni_page.dart';
+
+
+class AddTestimoniPage extends StatefulWidget {
+  const AddTestimoniPage({super.key});
+
+  @override
+  State<AddTestimoniPage> createState() => _AddTestimoniPageState();
+}
+
+class _AddTestimoniPageState extends State<AddTestimoniPage> {
+  final _formKey = GlobalKey<FormState>();
+  String nama = "";
+  String target = "";
+  String pesan = "";
+  
+  @override
+      Widget build(BuildContext context) {
+          return Scaffold(
+              appBar: AppBar(
+                  title: const Text('Testimoni Baru'),
+              ),
+              // drawer: const NavigatorDrawer(),
+              body: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: "Nama",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  )
+                                ),
+                                onChanged: (String? value){
+                                  setState(() {
+                                    nama = value!;
+                                  });
+                                },
+                                validator: (String? value){
+                                  if(value == null || value.isEmpty){
+                                    nama = "Anonymous";
+                                  }
+                                  return null;
+                                }          
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: "Target",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  )
+                                ),
+                                onChanged: (String? value){
+                                  setState(() {
+                                    target = value!;
+                                  });
+                                },
+                                validator: (String? value){
+                                  if(value == null || value.isEmpty){
+                                    target = "-";
+                                  }
+                                  return null;
+                                }          
+                              ),
+                              const SizedBox(height: 10),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: "Pesan",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  )
+                                ),
+
+                                onChanged: (String? value){
+                                  setState(() {
+                                    pesan = value!;
+                                  });
+                                },
+                                validator: (String? value){
+                                  if(value == null || value.isEmpty){
+                                    return 'Pesan tidak boleh kosong!';
+                                  }
+                                  else if(value.length <= 2){
+                                    return 'Isi pesan';
+                                  }
+                                  return null;
+                                }          
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const Text("Notes:\n- Default nama: Anonymous\n- Jika bukan resipien, maka abaikan target",
+                        style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
+                        ElevatedButton(
+                          onPressed: () {
+                            if(_formKey.currentState!.validate()){
+                              // addTestimoni(nama, pesan, target);
+                              Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => const TestimoniPage()));
+                            }
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(Colors.white)
+                          ),
+                          child: const Text("Buat Testimoni Baru", style: TextStyle(color: Colors.black)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+          );
+      }
+}
